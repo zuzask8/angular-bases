640143895d4ae33d964b904ee846f433ff593664
Miguel Zuza Cano <josemiguel.zuzacano@emeal.nttdata.com>
2024-08-27 13:15:05 +0200

    primer commit

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,23 @@
+import {  Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }]
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  deleteCharacterById(id: string) {
+  this.onDelete.emit(id)
+  }
+}
